4e954107f499ae57b5d1131ebe743b1337801640
Insik Cho <insik.cho@alaskaair.com>
2017-09-13 22:45:29 -0700

    Server side Render with express, ejs, webpack.config.js

---
+++ src/app-client.js
@@ -0,0 +1,9 @@
+// src/app-client.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+window.onload = () => {
+  ReactDOM.render(<App/>, document.getElementById('main'));
+
+};
---
+++ src/server.js
@@ -0,0 +1,37 @@
+// src/server.js
+
+import path from 'path';
+import { Server  } from 'http';
+import Express from 'express';
+import React from 'react';
+import { renderToString  } from 'react-dom/server';
+import App from './components/App';
+
+// initialize the server and configure support for ejs templates
+const app = new Express();
+const server = new Server(app);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// define the folder that will be used for static assets
+app.use(Express.static(path.join(__dirname, 'static')));
+
+// universal routing and rendering
+app.get('*', (req, res) => {
+  // generate the React markup for the current route
+  let markup;
+  markup = renderToString(<App />);
+
+  // render the index template with the embedded React markup
+  return res.render('index', { markup  });
+});
+
+// start the server
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
+server.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Server running on http://localhost:${port} [${env}]`);
+});
